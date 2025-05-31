d39b31003c3ee5618adef911ce4d401d08dc8c4f
achrefbenammar404 <achref.benammar404@gmail.com>
2025-05-31 19:45:43 +0100

    Feat: Implement friend relationship in User model and add addFriend mutation

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -8,4 +8,17 @@ export class UsersService extends BaseService<User> {
   constructor(prisma: PrismaService) {
     super(prisma, 'user');
   }
+  async addFriend(userId: string, friendId: string): Promise<User> {
+    return this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        friends: {
+          connect: { id: friendId },
+        },
+      },
+      include: {
+        friends: true,
+      },
+    });
+  }
 }
